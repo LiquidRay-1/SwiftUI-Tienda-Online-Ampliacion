7d2b262a71c4a80299331f0f44f102a6ed313095
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-03-13 20:48:28 +0100

    Update PagoView.swift

--- TiendaUI/TiendaUI/Views/PagoView.swift
+++ TiendaUI/TiendaUI/Views/PagoView.swift
@@ -2,7 +2,7 @@
 //  PagoView.swift
 //  TiendaUI
 //
-//  Created by Liquid on 13/3/24.
+//  Created by Ivan on 13/3/24.
 //
 
 import SwiftUI
@@ -17,14 +17,9 @@ struct PagoView: View {
     @State private var addCuponDetails = false
     @State private var cuponCode = ""
     
-    let propinas = [0, 5, 10, 15, 20]
-    
-    @State private var propina = 5
-    
     var totalPrice: String {
         let total = Double(lista.total)
-        let totalPropina = total/100 * Double(propina)
-        return (total + totalPropina).formatted(.currency(code: "EUR"))
+        return (total).formatted(.currency(code: "EUR"))
     }
     
     @State private var showingPaymentAlert = false
@@ -44,14 +39,7 @@ struct PagoView: View {
                     TextField("Introduce tu cupon", text: $cuponCode)
                 }
             }
-            Section("¿Dejar propina?") {
-                Picker("Porcentaje de propina", selection: $propina){
-                    ForEach(propinas, id: \.self){
-                        Text("\($0)%")
-                    }
-                }
-                .pickerStyle(.segmented)
-            }
+    
             Section("Total: \(totalPrice)"){
                 Button("Confirmar pedido"){
                     showingPaymentAlert.toggle()
